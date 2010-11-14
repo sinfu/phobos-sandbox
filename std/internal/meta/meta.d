d23ae2336c163c4639d2d6794269a44dec3ad8e1
sinfu <rsinfu@gmail.com>
2010-11-15 06:28:56 +0900

    meta.intersection: (doc) Elaborated degeneracy condition.

--- std/internal/meta/meta.d
+++ std/internal/meta/meta.d
@@ -5161,7 +5161,8 @@ Params:
 
 Returns:
  Sequence composed only of common elements of all the given sequences.  The
- empty sequence is returned if no sequence is passed, i.e. $(D seqs) is empty.
+ empty sequence is returned if no sequence is passed or at least one sequence
+ is empty.
 
  If the sequences contain $(D m1,m2,...) duplicates of the same entity
  respectively, the resulting _intersection will contain $(D min(m1,m2,...)),
