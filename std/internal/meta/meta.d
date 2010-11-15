2b1f1950f7381273704bd1728b44d37dd5fc10d3
sinfu <rsinfu@gmail.com>
2010-11-15 13:14:19 +0900

    meta.setify: Fixed a typo in ddoc example.

--- std/internal/meta/meta.d
+++ std/internal/meta/meta.d
@@ -5064,7 +5064,7 @@ Example:
 alias meta.setify!(int, bool, double, int) A;
 alias meta.setify!(bool, bool, double, int) B;
 
-static assert(is(A == TypeSeq!(bool, int, double, int)));
+static assert(is(A == TypeSeq!(bool, double, int, int)));
 static assert(is(B == TypeSeq!(bool, bool, double, int)));
 
 // Use meta.uniq to ignore the duplicates.
